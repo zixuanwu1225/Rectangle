a6dd1459b63e162d2823b7c65707937b14b4b321
wzixu <zwu2542@bths.edu>
2022-09-26 12:27:57 -0400

    Created code for cost of boards and windows and made code for end result of taxes and total cost of both.

---
+++ src/ConstructionPrice.java
@@ -0,0 +1,15 @@
+
+
+public class ConstructionPrice
+{
+    public double boardPrice;
+    public double windowPrice;
+    public double taxRate;
+
+    public ConstructionPrice(double boardPrice, double windowPrice, double taxRate)
+    {
+        this.boardPrice = boardPrice;
+        this.windowPrice = windowPrice;
+        this.taxRate = taxRate;
+    }
+}
